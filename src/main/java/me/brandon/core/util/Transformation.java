a212015253ab284a2d456c81ac3fd8fce08ebf05
Brandon <brandonshen123@gmail.com>
2024-05-31 17:18:44 -0700

    Added Camera Movement and also now 3d

--- src/main/java/me/brandon/core/util/Transformation.java
+++ src/main/java/me/brandon/core/util/Transformation.java
@@ -1,7 +1,9 @@
 package me.brandon.core.util;
 
+import me.brandon.core.Camera;
 import me.brandon.core.entity.Entity;
 import org.joml.Matrix4f;
+import org.joml.Vector3f;
 
 public class Transformation {
     public static Matrix4f createTransformationMatrix(Entity entity) {
@@ -11,6 +13,18 @@ public class Transformation {
                 rotateY((float) Math.toRadians(entity.getRotation().y)).
                 rotateZ((float) Math.toRadians(entity.getRotation().z)).
         scale(entity.getScale());
+        return matrix;
+    }
+
+    public static Matrix4f getViewMatrix(Camera camera){
+        Vector3f pos = camera.getPosition();
+        Vector3f rot = camera.getRotation();
+        Matrix4f matrix = new Matrix4f();
+        matrix.identity();
+        matrix.rotation((float) Math.toRadians(rot.x), new Vector3f(1,0,0))
+                .rotate((float) Math.toRadians(rot.y), new Vector3f(0,1,0))
+                .rotate((float) Math.toRadians(rot.z), new Vector3f(0,0,1));
+        matrix.translate(-pos.x, -pos.y, -pos.z);
         return matrix;
     }
 }
